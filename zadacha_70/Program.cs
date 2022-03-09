f553305984e6d561b81e4dfd3bac22b4291828bc
Larina <Losiknor@gmail.com>
2022-03-09 13:51:38 +0700

    Задача 70

---
+++ zadacha_70/Program.cs
@@ -0,0 +1,11 @@
+﻿// 70. Найти сумму цифр числа
+int SumDigits(int number)
+{
+    if(number < 1) return 0;
+    int result = 0;
+    result = SumDigits(number / 10) + number % 10;
+    return result;
+}
+Console.Write("Input number: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Sum digits: {SumDigits(number)}");
