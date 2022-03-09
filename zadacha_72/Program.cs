6da3fac6d171dc88f973029579e2bb9b085c217a
Larina <Losiknor@gmail.com>
2022-03-09 21:54:18 +0700

    Задача 72

---
+++ zadacha_72/Program.cs
@@ -0,0 +1,8 @@
+﻿// 70. Написать программу возведения числа А в целую стень B
+int DegreeOfNumber(int numberA, int numberB)
+{
+    if (numberB < 1) return 1;
+    return numberA * DegreeOfNumber(numberA, numberB-1);
+}
+Console.WriteLine(DegreeOfNumber(2, 10));
+//Console.WriteLine(DegreeOfNumber(new Random().Next(2, 9), new Random().Next(2, 9)));
