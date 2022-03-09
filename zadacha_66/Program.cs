7a4dc7d58f3c70cc3d5e6a287e970a3a66453fb5
Larina <Losiknor@gmail.com>
2022-03-09 13:23:27 +0700

    Задача 66

---
+++ zadacha_66/Program.cs
@@ -0,0 +1,18 @@
+﻿// 66. Показать натуральные числа от 1 до N, N задано
+// int N = new Random().Next(10, 99)
+// int showNat (int N)
+// {
+//     if (N < 1)
+//         break;
+//     Console.WriteLine(N);
+//     return showNat(N-1);
+// }
+// showNat (new Random().Next(10, 99));
+
+void PrintNumber(int n)
+{
+    if (n < 1) return;
+    PrintNumber(n - 1);
+    Console.Write(n + " ");
+}
+PrintNumber(new Random().Next(10, 99));
