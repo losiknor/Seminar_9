6e7eb4d4cbe663c07aa59eb923833f88c234108e
Larina <Losiknor@gmail.com>
2022-03-09 13:33:21 +0700

    Задача 68

--- zadacha_68/Program.cs
+++ zadacha_68/Program.cs
@@ -1,1 +1,9 @@
 ﻿// 68. Показать натуральные числа от M до N, N и M заданы
+void PrintNumber(int n, int m)
+{
+    if (n < m) return;
+    PrintNumber(n - 1, m);
+    Console.Write(n + " ");
+}
+PrintNumber(new Random().Next(20, 99), new Random().Next(1, 19));
+// PrintNumber(20, 2);
