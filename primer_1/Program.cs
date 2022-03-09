fa3f7f6c4466d4f8baa8eb01586d1715e6f8b4d1
Larina <Losiknor@gmail.com>
2022-03-09 11:53:32 +0700

    Пример 1

---
+++ primer_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Возвести 2 в степень N, применяя рекурсию
+int func(int N)
+{
+    if (N < 1)
+        return 1;
+
+    return 2*func(N-1);
+}
+
+Console.WriteLine(func(0));
+Console.WriteLine(func(2));
+Console.WriteLine(func(3));
+Console.WriteLine(func(4));
+Console.WriteLine(func(10));
