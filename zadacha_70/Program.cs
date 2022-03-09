bdb6a8d2db42a3151d3215259447a2baef18ffe9
Larina <Losiknor@gmail.com>
2022-03-09 13:58:23 +0700

    Задача 70_поправила

--- zadacha_70/Program.cs
+++ zadacha_70/Program.cs
@@ -2,9 +2,7 @@
 int SumDigits(int number)
 {
     if(number < 1) return 0;
-    int result = 0;
-    result = SumDigits(number / 10) + number % 10;
-    return result;
+    return SumDigits(number / 10) + number % 10;
 }
 Console.Write("Input number: ");
 int number = int.Parse(Console.ReadLine());
